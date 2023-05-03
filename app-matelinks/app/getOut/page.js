ea6d5a67e25eaad38b7d8b4fae0b8e218aa348c0
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-05-02 21:43:25 -0300

    first commit
    El comienzo ...
    to be continued...

---
+++ app-matelinks/app/getOut/page.js
@@ -0,0 +1,9 @@
+
+
+export default function getOut() {
+    return (
+        <div>
+            <h1>Bye Bye World!</h1>
+        </div>
+    )
+}
---
+++ app-matelinks/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+        <h1>Root</h1>
+      </div>
+    </main>
+  )
+}
