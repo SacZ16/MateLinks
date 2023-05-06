2b6e7ccd9f3f4691fe030ad4e909d4268cdb5235
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-05-06 18:51:40 -0300

    .

--- app-matelinks/app/hello/page.js
+++ app-matelinks/app/hello/page.js
@@ -1,9 +1,11 @@
 
 
-export default function page() {
+export default async function page() {
+    const res = await fetch(`/api/hello`)
+  const post = await res.json()
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1>{post.hello}</h1>
         </div>
     )
 }
